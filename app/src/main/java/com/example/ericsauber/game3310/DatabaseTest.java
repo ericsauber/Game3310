55e845185d407e5c028273b1a3499dd7c11fe72e
ericsauber <eric.sauber@mavs.uta.edu>
2017-04-22 17:14:39 -0500

    added database test activity

--- app/src/main/java/com/example/ericsauber/game3310/DatabaseTest.java
+++ app/src/main/java/com/example/ericsauber/game3310/DatabaseTest.java
@@ -5,12 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class Login extends AppCompatActivity {
+public class DatabaseTest extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_database_test);
     }
 
     public void gotoMain(View view) {
--- app/src/main/java/com/example/ericsauber/game3310/Main.java
+++ app/src/main/java/com/example/ericsauber/game3310/Main.java
@@ -1,7 +1,9 @@
 package com.example.ericsauber.game3310;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void gotoDBtest(View view) {
+
+        Intent intent = new Intent(this, DatabaseTest.class);
+        startActivity(intent);
     }
 }
