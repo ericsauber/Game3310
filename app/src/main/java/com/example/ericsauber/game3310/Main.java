3488a0d8e3cf707843369102895fbdba92f2c0e9
Nohemi Gonzalez <nohemi@Nohemis-MBP.T-mobile.com>
2017-04-24 23:36:40 -0500

    creating db, test1

--- app/src/main/java/com/example/ericsauber/game3310/Main.java
+++ app/src/main/java/com/example/ericsauber/game3310/Main.java
@@ -1,16 +1,42 @@
 package com.example.ericsauber.game3310;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class Main extends AppCompatActivity {
-
+    private static final String TAG = "Main";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SQLiteDatabase sqLiteDatabase = getBaseContext().openOrCreateDatabase("sqlite-db-1.db",MODE_PRIVATE,null);
+        String sql = "CREATE TABLE contacts(name TEXT, phone INTEGER);";
+        Log.d(TAG, "onCreate: sql is " + sql);
+
+        sqLiteDatabase.execSQL(sql);
+
+        sql= "INSERT INTO contacts VALUES('nohemi','1234567890');";
+        sqLiteDatabase.execSQL(sql);
+        sql = "INSERT INTO contacts VALUES('test','213233333');";
+        sqLiteDatabase.execSQL(sql);
+
+
+        Cursor query = sqLiteDatabase.rawQuery("SELECT * FROM contacts",null);
+        if(query .moveToFirst()){
+            String name = query.getString(0);
+            int phone = query.getInt(1);
+            Toast.makeText(this, "Name = " +name + " phone = " + phone, Toast.LENGTH_LONG).show();
+
+        }
+        query.close();
+        sqLiteDatabase.close();
     }
 
     public void gotoDBtest(View view) {
