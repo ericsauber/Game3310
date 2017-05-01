0f4c9fc76dfe595429a24a184906c42b565cb77b
Nohemi Gonzalez <nohemi@Nohemis-MBP.T-mobile.com>
2017-05-01 01:57:59 -0500

    highscore layout fixing

--- app/src/main/java/com/example/ericsauber/game3310/HighscoreTable.java
+++ app/src/main/java/com/example/ericsauber/game3310/HighscoreTable.java
@@ -2,12 +2,14 @@ package com.example.ericsauber.game3310;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -15,16 +17,21 @@ import java.util.ArrayList;
 
 public class HighscoreTable extends AppCompatActivity {
     MyDBHandler dbHandler;
+    TextView t;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_highscoretable);
+        t=(TextView) findViewById(R.id.DBtest_text);
 
         ListView listview=(ListView)findViewById(R.id.listView);
+        Typeface font=Typeface.createFromAsset(getAssets(),"fonts/MOUSETRAP!.otf");
+        t.setTypeface(font);
         dbHandler = new MyDBHandler(this, null, null, 1);
-
         ArrayList<String> thelist= new ArrayList<>();
         Cursor recordSet = dbHandler.getCursor();
+
 
 
         if(recordSet.getCount() == 0){
@@ -44,8 +51,11 @@ public class HighscoreTable extends AppCompatActivity {
                 String name = recordSet.getString(0);
                 Name_num = name + "    :    " + Integer.toString(num);
                 thelist.add(Name_num);
+
                 ListAdapter listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, thelist);
+
                 listview.setAdapter(listAdapter);
+
                 i++;
             }
         }
